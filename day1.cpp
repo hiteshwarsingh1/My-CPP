99b0486f29b42a3ee86ea8830086d28bf9c188db
Hiteshwar Singh <70597087+hiteshwarsingh1@users.noreply.github.com>
2020-11-24 18:12:04 +0530

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
 using namespace std;
-extern int h=8;
+void swap(int *x,int *y);
 int main(){
-	int a=10;
-//	cout<<a;
-	typedef int bla;
-	bla b=a;
-	cout<<h;
-//	cout<<b;
-	enum color{blue,red=4,green} c;
-	c=green;
+//	int a=10;
+////	cout<<a;
+//	typedef int bla;
+//	bla b=a;
+//	cout<<h;
+////	cout<<b;
+//	enum color{blue,red=4,green} c;
+//	c=green;
 //	cout<<c;
+//conditonal formatting
+	int a=10;
+	int b=20;
+	int c=10;
+//	a==b?cout<<"True1":cout<<"False1";
+//	a==c?cout<<"True2":cout<<"False2";
+//	cout<<b<<" "<<a<<"\n";
+	cout<<"f"<<&a<<"\n";
+	swap(&a,&b);
+	cout<<"l"<<&a;
+//	cout<<b<<" "<<a;
+//	cout<<*&a;
+}
+void swap(int *x,int *y){
+	cout<<x<<" "<<*x<<" "<<&x<<"\n";
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+	//return;
+	return;
 }
